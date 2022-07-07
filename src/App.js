2489e241aa7e61d6e760465c33acd9e4ab66114d
KimSeYoung <sagekim6@gmail.com>
2022-07-07 19:36:57 +0900

    Import emotion image files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./style/app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <h2>App 페이지</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Diary.js
@@ -0,0 +1,5 @@
+const Diary = () => {
+  return <div>Diary page</div>;
+};
+
+export default Diary;
